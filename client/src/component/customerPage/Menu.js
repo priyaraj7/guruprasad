3ec46fd273841010522094a333d7006e9d73e559
Priyaraj7 <suppichala@gmail.com>
2022-11-01 12:16:17 -0700

    update query

--- client/src/component/customerPage/Menu.js
+++ client/src/component/customerPage/Menu.js
@@ -65,63 +65,108 @@ const ItemContent = ({ children }) => {
 
 export default function Menu({ category }) {
   const [item, setItem] = useState([]);
+  const [earlyBreakfast, setEarlyBreakfast] = useState([]);
+  const [breakfast, setBreakfast] = useState([]);
+  const [supper, setSupper] = useState([]);
+  const [beverages, setBeverages] = useState([]);
 
   //   const getCategory = async (id) => setItem(await getAllItem(id));
   //   useEffect(() => {
   //     category.forEach(getCategory);
   //   }, [category]);
 
+  //   useEffect(() => {
+  //     let controller = new AbortController();
+  //     debugger;
+  //     (async (id) => {
+  //       try {
+  //         const url = new URL("/api/getByCategories", document.location);
+  //         [1, 2, 3, 4].forEach((id) => url.searchParams.append("categoryId", id));
+  //         const response = await fetch(url.toString(), {
+  //           signal: controller.signal,
+  //         });
+  //         setItem(await response.json());
+  //         controller = null;
+  //       } catch (e) {
+  //         // Handle fetch error
+  //         console.log(e);
+  //       }
+  //     })();
+  //     return () => controller?.abort;
+  //   }, []);
+
+  //console.log(item);
+
+  const getEarlyBreakfast = async () => {
+    setEarlyBreakfast(await getAllItem(1));
+  };
+
   useEffect(() => {
-    let controller = new AbortController();
-    debugger;
-    (async (id) => {
-      try {
-        const url = new URL("/api/getByCategories", document.location);
-        [1, 2, 3, 4].forEach((id) => url.searchParams.append("categoryId", id));
-        const response = await fetch(url.toString(), {
-          signal: controller.signal,
-        });
-        setItem(await response.json());
-        controller = null;
-      } catch (e) {
-        // Handle fetch error
-        console.log(e);
-      }
-    })();
-    return () => controller?.abort;
+    getEarlyBreakfast();
   }, []);
 
-  console.log(item);
+  const getBreakfast = async () => {
+    setBreakfast(await getAllItem(2));
+  };
+
+  useEffect(() => {
+    getBreakfast();
+  }, []);
+
+  const getSupper = async () => {
+    setSupper(await getAllItem(3));
+  };
+
+  useEffect(() => {
+    getSupper();
+  }, []);
+
+  const getBeverages = async () => {
+    setBeverages(await getAllItem(4));
+  };
+
+  useEffect(() => {
+    getBeverages();
+  }, []);
+
+  console.log(earlyBreakfast);
+  let arr = [earlyBreakfast, breakfast, supper, beverages];
+  // console.log(arr);
 
   return (
-    <Box bg={useColorModeValue("gray.100", "gray.700")}>
-      <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
-        <Stack spacing={0} align={"center"}>
-          <Heading>Early Breakfast</Heading>
-          <Text>6.30am to 12.00pm</Text>
-        </Stack>
-        <Stack
-          direction={{ base: "column", md: "row" }}
-          spacing={{ base: 10, md: 4, lg: 10 }}
-        >
-          <ItemBox>
-            <ItemContent>
-              <Heading size="md">Idli</Heading>
-              <Flex minWidth="max-content" alignItems="center" gap="2">
-                <Box p="2">
-                  <Text size="md">
-                    <Icon as={FaRupeeSign} />
-                    100
-                  </Text>
-                </Box>
-                <Spacer />
+    <>
+      {arr.map((category, index) => {
+        return <p>{category[index].item_name}</p>;
+      })}
+      <Box bg={useColorModeValue("gray.100", "gray.700")}>
+        <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+          <Stack spacing={0} align={"center"}>
+            <Heading>Early Breakfast</Heading>
+            <Text>6.30am to 12.00pm</Text>
+          </Stack>
+          <Stack
+            direction={{ base: "column", md: "row" }}
+            spacing={{ base: 10, md: 4, lg: 10 }}
+          >
+            <ItemBox>
+              <ItemContent>
+                <Heading size="md">Idli</Heading>
+                <Flex minWidth="max-content" alignItems="center" gap="2">
+                  <Box p="2">
+                    <Text size="md">
+                      <Icon as={FaRupeeSign} />
+                      100
+                    </Text>
+                  </Box>
+                  <Spacer />
 
-                <Button colorScheme="teal">Order Now</Button>
-              </Flex>
-            </ItemContent>
-          </ItemBox>
-        </Stack>
-      </Container>
-    </Box>
+                  <Button colorScheme="teal">Order Now</Button>
+                </Flex>
+              </ItemContent>
+            </ItemBox>
+          </Stack>
+        </Container>
+      </Box>
+    </>
   );
 }
--- server/src/models/itemModel.js
+++ server/src/models/itemModel.js
@@ -8,11 +8,22 @@ export function initDb() {
 
 export const itemModel = async (id) => {
   const { rows: items } = await db.query(
-    `SELECT item.id, item.category_id,item.item_name, item.price, item.active, category.category_name 
+    `SELECT item.id, item.category_id, item.item_name, item.price, category.category_name, category.description
     FROM 
     item INNER JOIN 
     category on item.category_id = category.id 
-    WHERE category_id = ${id};`
+    WHERE category_id = ${id} AND item.active = TRUE
+    ORDER BY id ASC;`
   );
-  return items;
+  if (items.length) {
+    const result = {
+      categoryId: items[0].category_id,
+      categoryName: items[0].category_name,
+      description: items[0].description,
+      items,
+    };
+    return result;
+  }
+
+  return null;
 };
