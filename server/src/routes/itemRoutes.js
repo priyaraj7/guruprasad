de08678c967f250e660b1e90ae58d0ba7c6cb454
Priyaraj7 <suppichala@gmail.com>
2022-11-03 10:14:37 -0700

    delete commentout code

--- server/src/routes/itemRoutes.js
+++ server/src/routes/itemRoutes.js
@@ -3,10 +3,6 @@ import { itemController } from "../controller/itemController.js";
 
 const router = express.Router();
 
-// router.get("/getByCategories", async (req, resp) => {
-//   console.log(req.query.categoryId);
-//   return resp.send("OK -" + req.query.categoryId.join(", "));
-// });
 router.get("/:categoryId", itemController);
 
 export default router;
