e1ff1edcf53d2a3b612aa5996959e1aca799cd8a
Priyaraj7 <suppichala@gmail.com>
2022-11-03 14:39:47 -0700

    clean up the sql query

--- server/src/models/itemModel.js
+++ server/src/models/itemModel.js
@@ -6,50 +6,25 @@ export function initDb() {
   db = initDBConnection();
 }
 
-// export const itemsByCategory = async () => {
-//   const { rows: items } = await db.query(
-//     `SELECT item.id, item.category_id, item.item_name, item.price, category.category_name, category.description
-//     FROM
-//     item INNER JOIN
-//     category on item.category_id = category.id
-//     WHERE  item.active = TRUE
-//     ORDER BY id ASC;`
-//   );
-//   if (items.length) {
-//     const result = {
-//       categoryId: items.category_id,
-//       categoryName: items.category_name,
-//       description: items.description,
-//       items,
-//     };
-//     return result;
-//   }
-
-//   return null;
-// };
-
 export const itemsByCategory = async () => {
   const { rows: items } = await db.query(
-    `select * 
-    from(
-      select c.id, c.category_name, c.description, 
-      (select json_agg(ite)
-      from ( 
-        select id, item_name As itemName, price  from item where category_id = c.id AND active = TRUE
-      ) ite 
-    ) as item
-    from category as c) category
-    ORDER BY category.id ASC;`
+    `SELECT
+    C.ID,
+    C.CATEGORY_NAME,
+    C.DESCRIPTION,
+   (
+        SELECT json_agg(json_build_object(
+            'id', ID,
+            'itemname', ITEM_NAME,
+            'price', PRICE
+        ))
+        FROM ITEM
+        WHERE
+            CATEGORY_ID = C.ID AND ACTIVE IS TRUE
+    ) AS  ITEM    
+FROM CATEGORY C
+ORDER BY C.ID ASC;`
   );
-  // if (items.length) {
-  //   const result = {
-  //     categoryId: items.category_id,
-  //     categoryName: items.category_name,
-  //     description: items.description,
-  //     items,
-  //   };
-  //   return result;
-  // }
 
-  return items;
+  return items.length ? items : "NO items";
 };
