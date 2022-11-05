c19be0eda7ab38621f2319776ae854b8e938b37c
Priyaraj7 <suppichala@gmail.com>
2022-11-04 22:07:38 -0700

    update google API file

--- client/src/api/googleApi.js
+++ client/src/api/googleApi.js
@@ -1,20 +1,6 @@
-const handleError = function (error) {
-  console.warn(error);
-  return new Response(
-    JSON.stringify({
-      code: 400,
-      message: "Network Error",
-    })
-  );
-};
-
 export const getReviews = async () => {
-  const response = await fetch(`/place/reviews`).catch(handleError);
+  const response = await fetch(`/place/reviews`);
   const result = await response.json();
 
-  if (response.ok) {
-    return result;
-  } else {
-    return Promise.reject(response);
-  }
+  return result;
 };
