1a12f2ad30206d152331a225700aefb5ea48d0a9
Priyaraj7 <suppichala@gmail.com>
2022-11-07 15:14:24 -0800

    auth0 is working with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import "./App.css";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // COMPONENTS
 
@@ -11,14 +12,34 @@ import Menu from "./component/customer/Menu";
 import ContactPage from "./component/customer/ContactPage";
 import ReviewPage from "./component/customer/ReviewPage";
 
+// Auth0
+import LoginButton from "./component/LoginButton";
+import LogoutButton from "./component/LogoutButton";
+import Profile from "./component/Profile";
+
 function App() {
+  const { isLoading, error } = useAuth0();
+
   const location = useLocation();
   const isAdmin = location.pathname.startsWith("/admin");
   return (
     <ChakraProvider>
       <CSSReset />
 
-      <Header links={isAdmin ? ADMIN_LINKS : CUSTOMER_LINKS} />
+      <main className="column">
+        <h1>Auth0 Learning</h1>
+        {error && <p>Authentication Error</p>}
+        {!error && isLoading && <p>Loading.....</p>}
+        {!error && !isLoading && (
+          <>
+            <LoginButton />
+            <LogoutButton />
+            <Profile />
+          </>
+        )}
+      </main>
+
+      {/* <Header links={isAdmin ? ADMIN_LINKS : CUSTOMER_LINKS} />
       <Routes>
         <Route index element={<Menu />} />
         <Route path="/contact" element={<ContactPage />} />
@@ -26,7 +47,7 @@ function App() {
         <Route path="admin" element={<AdminPage />}>
           <Route path="messages" element={<MessagesPage />} />
         </Route>
-      </Routes>
+      </Routes> */}
     </ChakraProvider>
   );
 }
---
+++ client/src/component/LogoutButton.js
@@ -0,0 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+function LogoutButton() {
+  const { logout, isAuthenticated } = useAuth0();
+
+  return isAuthenticated && <button onClick={() => logout()}>Sign out</button>;
+}
+
+export default LogoutButton;
