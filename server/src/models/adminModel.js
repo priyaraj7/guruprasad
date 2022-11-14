2260d20afb4662455eef4196163b4d94e0f349b3
Priyaraj7 <suppichala@gmail.com>
2022-11-14 11:48:10 -0800

    delete commentout code

--- server/src/models/adminModel.js
+++ server/src/models/adminModel.js
@@ -1,14 +1,3 @@
-//import { initDBConnection } from "../../db/db-connection.js";
-
-// let db;
-
-// This function is because I got the error query is undefined
-// function ensureDb() {
-//   if (!db) {
-//     db = initDBConnection();
-//   }
-// }
-
 const frontendDataQuery = `SELECT
  item.id,
  item_name AS itemName,
@@ -29,6 +18,11 @@ export default function (db) {
       return items;
     },
 
+    // https://stackoverflow.com/questions/59232370/adding-a-left-join-on-a-insert-into-returning
+    //   /* A WITH query allows a record returned from an initial query,
+    //   including data returned from a RETURNING clause, which is stored in a
+    //   temporary table that can be accessed in the expression that follows
+    //   it to continue work on it, including using a JOIN expression. */
     addItem: async (item) => {
       const insertQuery = `
     with item as (INSERT INTO
@@ -37,11 +31,9 @@ export default function (db) {
     ${frontendDataQuery}`;
       const values = [item.itemName, item.price, item.categoryId];
 
-      // console.log(`query ====> ${insertQuery}`);
       return await db.query(insertQuery, values);
     },
     toggleActiveStatus: async (id) => {
-      // debugger;
       return await db.query(
         `UPDATE item SET active = NOT active WHERE id = ${id} RETURNING *`
       );
@@ -54,7 +46,6 @@ export default function (db) {
     },
 
     getMessage: async () => {
-      // debugger;
       const getQuery = `SELECT * FROM message`;
       return await db.query(getQuery);
     },
@@ -64,56 +55,3 @@ export default function (db) {
     },
   };
 }
-
-// export  const getAllItem = async () => {
-//   // ensureDb();
-//   // debugger;
-//   // console.log(db);
-//   const { rows: items } = await db.query(frontendDataQuery);
-
-//   return items;
-// };
-
-// export const addItem = async (item) => {
-//   ensureDb();
-//   // https://stackoverflow.com/questions/59232370/adding-a-left-join-on-a-insert-into-returning
-//   /* A WITH query allows a record returned from an initial query,
-//   including data returned from a RETURNING clause, which is stored in a
-//   temporary table that can be accessed in the expression that follows
-//   it to continue work on it, including using a JOIN expression. */
-//   const insertQuery = `
-//   with item as (INSERT INTO
-//     item(item_name, price, category_id)
-//   VALUES($1, $2, $3) RETURNING *)
-//   ${frontendDataQuery}`;
-//   const values = [item.itemName, item.price, item.categoryId];
-
-//   // console.log(`query ====> ${insertQuery}`);
-//   return await db.query(insertQuery, values);
-// };
-
-// export const toggleActiveStatus = async (id) => {
-//   // debugger;
-//   return await db.query(
-//     `UPDATE item SET active = NOT active WHERE id = ${id} RETURNING *`
-//   );
-// };
-
-// export const updateItem = async (id, item) => {
-//   const query = `Update item SET item_name = $1, price = $2,category_id = $3 where id = $4 RETURNING *`;
-//   const values = [item.itemname, item.price, item.categoryId, id];
-//   return await db.query(query, values);
-// };
-
-// // FOR CONTACT PAGE
-// // get query
-// export const getMessage = async () => {
-//   // debugger;
-//   const getQuery = `SELECT * FROM message`;
-//   return await db.query(getQuery);
-// };
-
-// // Delete query
-// export const deleteMessage = async (id) => {
-//   return await db.query(`DELETE FROM message WHERE id = $1`, [id]);
-// };
